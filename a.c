adc022444830ef00e0a8ba2eb04294dfc9dadb6b
lin, chaochao <chaochaox.lin@intel.com>
2018-11-26 16:49:22 +0800

    add a.c

---
+++ a.c
@@ -0,0 +1,12 @@
+/*
+ * @file: a.c
+ * @author: Lin, Chao <chaochaox.lin@intel.com>
+ * @create time: 2018-11-26 16:48:51
+ * @last modified: 2018-11-26 16:48:51
+ * @description:
+ */
+#include <stdio.h>
+
+int main() {
+
+}
